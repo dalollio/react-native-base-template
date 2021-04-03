cc4cc727a02a8d4918d6e221545b0070e1ee4765
Carlos Rodrigues <dalollio.c@gmail.com>
2021-04-03 11:43:21 -0300

    :rocket: Add VectorIcons support

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useColorScheme } from "react-native-appearance";
 import { dark, light } from "./theme/index";
 import { NavigationContainer } from '@react-navigation/native';
 import { translate } from "./locales";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const src: React.FC = () => {
   const scheme = useColorScheme();
@@ -19,6 +20,7 @@ const src: React.FC = () => {
     /> 
     <NavigationContainer theme={theme}>
         <Container theme={theme}>
+            <Icon name='language' color={theme.colors.text}></Icon>
             <BaseText theme={theme}>{translate('hello')}</BaseText>
         </Container>
     </NavigationContainer>
