236075975c60f5819125467ac18e98ea501d6b37
Carlos Rodrigues <dalollio.c@gmail.com>
2021-04-07 17:59:02 -0300

    :rocket: Add Modalize

--- android/app/src/main/java/com/reactnativebase/MainActivity.java
+++ android/app/src/main/java/com/reactnativebase/MainActivity.java
@@ -2,7 +2,11 @@ package com.reactnativebase;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -21,5 +25,15 @@ public class MainActivity extends ReactActivity {
     Intent intent = new Intent("onConfigurationChanged");
     intent.putExtra("newConfig", newConfig);
     sendBroadcast(intent);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+      return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text } from 'react-native';
 import { useColorScheme } from 'react-native-appearance';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { Container } from './globalStyles';
 import Navigation from './navigation';
 import { dark, light } from './theme';
+import { Modalize } from 'react-native-modalize';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
     const scheme = useColorScheme();
     const theme = scheme === 'dark' ? dark : light;
+
   return (
     <SafeAreaProvider>
         <Container theme={theme} style={{backgroundColor:'#fff', flex:1}}>
             <Navigation />
         </Container>
+
     </SafeAreaProvider>
   )
 }
